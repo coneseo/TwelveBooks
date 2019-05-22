aa8b6b15a89934c4c488da64ea1b90f0b7fe4f63
coneseo <woon4754@gmail.com>
2019-05-22 21:24:45 +0900

    #20190522 WebClient, reactor, Mono 를 이용한 책 검색 기능 수정 완료

--- src/main/java/com/twelvebooks/twelvebook/service/KakaoBookApiService.java
+++ src/main/java/com/twelvebooks/twelvebook/service/KakaoBookApiService.java
@@ -1,8 +1,14 @@
 package com.twelvebooks.twelvebook.service;
 
+import com.twelvebooks.twelvebook.dto.SearchResultDTO;
+import reactor.core.publisher.Mono;
+
 import java.util.Map;
 
 public interface KakaoBookApiService {
     //책검색
-    public Map<String, Object> searchBooks(String searchWord, String target, String category, int page);
+//    public Map<String, Object> searchBooks(String searchWord, String target, String category, int page);
+
+    //
+    public Mono<SearchResultDTO> searchBooks(String searchWord, int page);
 }
--- src/main/java/com/twelvebooks/twelvebook/service/implementation/KakaoBookApiServiceImpl.java
+++ src/main/java/com/twelvebooks/twelvebook/service/implementation/KakaoBookApiServiceImpl.java
@@ -2,11 +2,14 @@ package com.twelvebooks.twelvebook.service.implementation;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.twelvebooks.twelvebook.Util.RestProperties;
-import com.twelvebooks.twelvebook.Util.Utils;
+import com.twelvebooks.twelvebook.dto.SearchResultDTO;
+import com.twelvebooks.twelvebook.util.RestProperties;
+import com.twelvebooks.twelvebook.util.Utils;
 import com.twelvebooks.twelvebook.service.KakaoBookApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,27 +19,43 @@ public class KakaoBookApiServiceImpl implements KakaoBookApiService {
     @Autowired
     private RestProperties restProperties;
 
+    @Autowired
+    WebClient.Builder webClientBuild;
+
     private static final String API_BOOK_URL = "https://dapi.kakao.com/v3/search/book";
 
-    @Override
-    public Map<String, Object> searchBooks(String searchWord, String target, String category, int page) {
-        final String URL = API_BOOK_URL + "?target=" + target +  "&category=" + category + "&page="
-                + page;
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Authorization", "KakaoAK " + restProperties.getProperty("admin.booksearch"));
-        Map<String, String> params = new HashMap<>();
-        params.put("query", searchWord);
-        String jsonString = null;
-        Map<String, Object> resultData = null;
-        try {
-            jsonString = Utils.getHttpPOST2String(URL, headers, params);
-            ObjectMapper mapper = new ObjectMapper();
-            resultData = mapper.readValue(jsonString, new TypeReference<Map<String, Object>>() {
-            });
+//    @Override
+//    public Map<String, Object> searchBooks(String searchWord, String target, String category, int page) {
+//        final String URL = API_BOOK_URL + "?target=" + target +  "&category=" + category + "&page="
+//                + page;
+//        Map<String, String> headers = new HashMap<>();
+//        headers.put("Authorization", "KakaoAK " + restProperties.getProperty("admin.booksearch"));
+//        Map<String, String> params = new HashMap<>();
+//        params.put("query", searchWord);
+//        String jsonString = null;
+//        Map<String, Object> resultData = null;
+//        try {
+//            jsonString = Utils.getHttpPOST2String(URL, headers, params);
+//            ObjectMapper mapper = new ObjectMapper();
+//            resultData = mapper.readValue(jsonString, new TypeReference<Map<String, Object>>() {
+//            });
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return resultData;
+//    }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return resultData;
+    @Override
+    public Mono<SearchResultDTO> searchBooks(String searchWord, int page) {
+
+        WebClient webClient = webClientBuild.baseUrl("https://dapi.kakao.com").build();
+
+        Mono<SearchResultDTO> searchResult = webClient.get().uri("/v3/search/book?query=" + searchWord + "&page="+page)
+                .header("Authorization", "KakaoAK " + restProperties.getProperty("admin.booksearch"))
+                .retrieve()
+                .bodyToMono(SearchResultDTO.class);
+
+        return searchResult;
     }
 }
