d3b74b3992485585c2522bc3fda550ea12ff0f29
coneseo <woon4754@gmail.com>
2019-05-22 18:29:37 +0900

    #20190522 책 검색 수정을 위한 DTO 수정

--- src/main/java/com/twelvebooks/twelvebook/dto/SearchBookDTO.java
+++ src/main/java/com/twelvebooks/twelvebook/dto/SearchBookDTO.java
@@ -1,15 +1,19 @@
 package com.twelvebooks.twelvebook.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
 public class SearchBookDTO {
+
     private String title;
     private List<String> authors;
     private String isbn;
     private List<String> translators;
     private String thumbnail;
     private String publisher;
-
-
 
 }
