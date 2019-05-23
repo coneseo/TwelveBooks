8db486d41432f94042e5e6f8bb22bba7052e7d00
coneseo <woon4754@gmail.com>
2019-05-23 16:37:27 +0900

    #20190523 webClient Bean으로 등록해서 사용

---
+++ src/main/java/com/twelvebooks/twelvebook/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.twelvebooks.twelvebook.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebConfig {
+
+    @Autowired
+    WebClient.Builder webClientBuild;
+
+    @Bean
+    public WebClient webClient(){
+        WebClient webClient = webClientBuild.baseUrl("https://dapi.kakao.com").build();
+       return webClient;
+    }
+}
